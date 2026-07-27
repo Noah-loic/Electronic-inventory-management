26719458bdfca563a649b9a04536486a0fca4b13
Noah Loic <noahloic0123@gmail.com>
2026-07-27 12:53:05 +0200

    initial commit

---
+++ EUCL/src/test/java/com/example/EUCL/EuclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EUCL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
